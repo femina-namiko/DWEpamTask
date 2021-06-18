24661b1fb1273dc7c156065623840e441d606570
femina-namiko <39432736+femina-namiko@users.noreply.github.com>
2021-06-18 13:27:47 +0200

    Update sales.ddl.sql

--- sales.ddl.sql
+++ sales.ddl.sql
@@ -2,9 +2,11 @@ DROP TABLE SalesDB.Sales.Product
 DROP TABLE SalesDB.Sales.Category
 DROP TABLE SalesDB.Sales.Sales
 DROP TABLE SalesDB.Sales.Customer
+DROP TABLE SalesDB.Sales.Calendar
 
 /* ************************CREATING TABLES********************** */
 
+--Existed dimension <Product>
 CREATE TABLE SalesDB.Sales.Product
 (
 ID INT PRIMARY KEY IDENTITY(1,1),
@@ -12,6 +14,7 @@ ProductName VARCHAR (50) NOT NULL,
 CategoryName VARCHAR (50) NOT NULL
 )
 
+--Existed dimension <Category>
 CREATE TABLE SalesDB.Sales.Category
 (
 ID INT PRIMARY KEY IDENTITY(1,1),
@@ -19,6 +22,7 @@ CategoryName VARCHAR (50) NOT NULL,
 Description VARCHAR (50)
 )
 
+--Existed fact table <Sales>
 CREATE TABLE SalesDB.Sales.Sales
 (
 ID INT PRIMARY KEY IDENTITY(1,1),
@@ -27,6 +31,7 @@ CategoryID INT NOT NULL,
 SalesValues INT
 )
 
+--New added dimension <Customer>
 CREATE TABLE SalesDB.Sales.Customer
 (
 ID INT PRIMARY KEY IDENTITY(1,1),
@@ -37,20 +42,42 @@ City VARCHAR (50),
 Country VARCHAR (50)
 )
 
+--New added dimension <Calendar>
+CREATE TABLE SalesDB.Sales.Calendar
+(
+ID INT PRIMARY KEY IDENTITY(1,1),
+CalendarDate VARCHAR (50) NOT NULL,
+CalendarYear VARCHAR (50),
+CalendarQuarter VARCHAR (50),
+CalendarMonth VARCHAR (50)
+)
+
+--Adding FK CustomerID to the table of facts  
 ALTER TABLE SalesDB.Sales.Sales
 ADD CustomerID INT
 
+--Adding FK CalendarID to the table of facts
+ALTER TABLE SalesDB.Sales.Sales
+ADD CalendarID INT 
+
 /* ****************CREATING REFERENCES******************* */
 
--- Reference: city_country (table: city)
+-- Reference: sales_product between tables "sales" and "product"
 ALTER TABLE SalesDB.Sales.Sales ADD CONSTRAINT sales_product
     FOREIGN KEY (ProductID)
     REFERENCES SalesDB.Sales.Product (ID)
 
+-- Reference: sales_category
 ALTER TABLE SalesDB.Sales.Sales ADD CONSTRAINT sales_category
     FOREIGN KEY (CategoryID)
     REFERENCES SalesDB.Sales.Category (ID)
 
+-- Reference: sales_customer
 ALTER TABLE SalesDB.Sales.Sales ADD CONSTRAINT sales_customer
     FOREIGN KEY (CustomerID)
     REFERENCES SalesDB.Sales.Customer (ID);
+
+-- Reference: sales_calendar
+ALTER TABLE SalesDB.Sales.Sales ADD CONSTRAINT sales_calendar
+    FOREIGN KEY (CalendarID)
+    REFERENCES SalesDB.Sales.Calendar (ID);
